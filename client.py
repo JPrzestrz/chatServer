232aeb436a6975559461f2daa99e85e4847c2007
s184850 <s184850@student.pg.edu.pl>
2023-04-13 16:07:51 +0200

    [Client] Updated username handling.

---
+++ client.py
@@ -0,0 +1,32 @@
+import socket
+import threading
+
+HOST = '127.0.0.1' # IP address of the server
+PORT = 5555 # Port number to connect to
+
+def receive_messages(sock):
+    """Function to receive messages from the server."""
+    while True:
+        data = sock.recv(1024) # Receive up to 1024 bytes of data
+        if not data:
+            break
+        message = data.decode('utf-8')
+        print(message)
+
+def start_client():
+    """Function to start the client program."""
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        print(f"Connected to server at {HOST}:{PORT}")
+        username = input("Enter your username: ")
+        s.sendall(username.encode('utf-8'))
+        # Start a thread to receive messages from the server
+        receive_thread = threading.Thread(target=receive_messages, args=(s,))
+        receive_thread.start()
+        # Start the main loop
+        while True:
+            message = input()
+            s.sendall(f"{username}: {message}".encode('utf-8'))
+
+if __name__ == '__main__':
+    start_client()
